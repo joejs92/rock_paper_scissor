aaf6644b1572d0a83ac66480391c8e6255e2d300
joejs92 <joejs_92@yahoo.com>
2023-08-10 02:25:19 -0400

    Complete project to specifications.

--- source.js
+++ source.js
@@ -17,80 +17,85 @@ function playRound(playerSelection){
         return computerChoice
     }
 
-    //function getPlayerSelection(){
-    //    let playerSelection = prompt("Choose either Rock, Paper, or Scissor.");
-    //    let firstLetter = playerSelection[0];
-    //    let otherText = playerSelection.slice(1);
-    //    let newText = otherText.toLowerCase();
-    //    let newLetter = firstLetter.toUpperCase();
-    //return newLetter+newText;
-    //}
-
     let computerSelection = getComputerChoice();
-    //let playerSelection = getPlayerSelection();
+    let result = "";
+    let brack = [];
 
     function winner(playerSelection,computerSelection){
         if (playerSelection == "Rock" && computerSelection=="Rock"){
             gamesPlayed +=1;
-            return "Tie! You both chose Rock."
+            result = "Tie! You both chose Rock."
         }
         else if (playerSelection == "Rock" && computerSelection=="Paper"){
             gamesPlayed +=1;
             computerScore +=1;
-            return "You Lose! Paper beats Rock."
+            result = "You Lose! Paper beats Rock."
         }
         else if (playerSelection == "Rock" && computerSelection=="Scissor"){
             gamesPlayed +=1;
             playerScore +=1;
-            return "You Win! Rock beats Scissor."
+            result = "You Win! Rock beats Scissor."
         }
         else if (playerSelection == "Paper" && computerSelection=="Rock"){
             gamesPlayed +=1;
             playerScore +=1;
-            return "You Win! Paper beats Rock."
+            result = "You Win! Paper beats Rock."
         }
         else if (playerSelection == "Paper" && computerSelection=="Paper"){
             gamesPlayed +=1;
-            return "Tie! You both chose Paper."
+            result = "Tie! You both chose Paper."
         }
         else if (playerSelection == "Paper" && computerSelection=="Scissor"){
             gamesPlayed +=1;
             computerScore +=1;
-            return "You Lose! Scissor beats Paper."
+            result = "You Lose! Scissor beats Paper."
         }
         else if (playerSelection == "Scissor" && computerSelection=="Paper"){
             gamesPlayed +=1;
             playerScore +=1;
-            return "You Win! Scissor beats Paper."
+            result = "You Win! Scissor beats Paper."
         }
         else if (playerSelection == "Scissor" && computerSelection=="Rock"){
             gamesPlayed +=1;
             computerScore +=1;
-            return "You Lose! Rock beats Scissor."
+            result = "You Lose! Rock beats Scissor."
         }
         else if (playerSelection == "Scissor" && computerSelection=="Scissor"){
             gamesPlayed +=1;
-            return "You Win! Scissor beats Paper."
+            playerScore += 1;
+            result = "You Win! Scissor beats Paper."
         }
-     
+        return brack = [result,gamesPlayed,playerScore,computerScore];
     }
-    return console.log(winner(playerSelection,computerSelection));
+    winner(playerSelection,computerSelection);
+    const content = document.createElement('div');
+    
+    const conResult = document.createElement('p');
+    content.appendChild(conResult);
+    conResult.textContent = `${brack[0]}`;
+
+    const conScore = document.createElement('p');
+    content.appendChild(conScore);
+    conScore.textContent = `Games played: ${brack[1]} Player score: ${brack[2]} Computer score: ${brack[3]}`;
+
+    container.appendChild(content);
+
+    if (playerScore == 5) {
+        const finalResult = document.createElement('p');
+        content.appendChild(finalResult);
+        finalResult.textContent = 'Congratulations! You Won!';
+    }
+    else if (computerScore == 5) {
+        const finalResult = document.createElement('p');
+        content.appendChild(finalResult);
+        finalResult.textContent = 'You lost and are awful.';
+    }
 }
 
-const btn1 = document.querySelector('#Rock');
-btn1.addEventListener('click', () => {
-    let playerSelection = 'Rock';
-    playRound(playerSelection);
-});
-
-const btn2 = document.querySelector('#Paper');
-btn2.addEventListener('click', () => {
-    let playerSelection = 'Paper';
-    playRound(playerSelection);
-});
-
-const btn3 = document.querySelector('#Paper');
-btn3.addEventListener('click', () => {
-    let playerSelection = 'Paper';
-    playRound(playerSelection);
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click',() => {
+        playerSelection = button.id;
+        playRound(playerSelection);
+    });
 });
